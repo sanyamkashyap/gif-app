faec8fe4285554fd0702f3e4620478b86522b96a
sanyamkashyap <sanyamkashyap40@gmail.com>
2025-08-16 10:40:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import AppLayout from './layouts/app-layout'
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />
+  }
+])
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
