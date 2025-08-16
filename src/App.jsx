d7e6c3e507ccfff828518d51a2234ed3b4dc2d3f
sanyamkashyap <sanyamkashyap40@gmail.com>
2025-08-16 10:58:06 +0530

    setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
+import { createBrowserRouter } from "react-router-dom"
 import './App.css'
 import AppLayout from './layouts/app-layout'
+import Categories from "./layouts/categories"
+import SearchPage from "./layouts/search"
+import Favorites from "./layouts/favorites"
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout />
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/:category',
+        element: <Categories />
+      },
+      {
+        path: '/search/:query',
+        element: <SearchPage />
+      },
+      {
+        path: '/:type/:slug',
+        element: <Home />
+      },
+      {
+        path: '/favorites',
+        element: <Favorites />
+      },
+    ]
   }
 ])
 
